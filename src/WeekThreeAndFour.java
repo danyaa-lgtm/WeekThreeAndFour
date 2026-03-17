ddd0514e0ee3766b43a56285f70db9bf8f556c91
danyaa-lgtm <pro.danyaa@gmail.com>
2026-03-17 09:35:57 +0530

    Level2 Ques3

--- src/WeekThreeAndFour.java
+++ src/WeekThreeAndFour.java
@@ -2,16 +2,10 @@ import java.util.Scanner;
 public class WeekThreeAndFour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter salary: ");
-        double salary = sc.nextDouble();
-        System.out.print("Enter years of service: ");
-        int years = sc.nextInt();
-        if (years > 5) {
-            double bonus = salary * 0.05;
-            System.out.println("Bonus amount: " + bonus);
-        } 
-        else {
-            System.out.println("Bonus amount: 0");
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
         }
     }
 }
