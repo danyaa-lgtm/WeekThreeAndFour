c7ec8f9bac53cee396242382d0a6b52cdfa4753d
danyaa-lgtm <pro.danyaa@gmail.com>
2026-03-17 09:31:55 +0530

    Level2 Ques2

--- src/WeekThreeAndFour.java
+++ src/WeekThreeAndFour.java
@@ -2,20 +2,16 @@ import java.util.Scanner;
 public class WeekThreeAndFour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        int number = sc.nextInt();
-        if (number > 0) {
-            for (int i = 1; i <= number; i++) {
-                if (i % 2 == 0) {
-                    System.out.println(i + " is an even number");
-                } 
-                else {
-                    System.out.println(i + " is an odd number");
-                }
-            }
+        System.out.print("Enter salary: ");
+        double salary = sc.nextDouble();
+        System.out.print("Enter years of service: ");
+        int years = sc.nextInt();
+        if (years > 5) {
+            double bonus = salary * 0.05;
+            System.out.println("Bonus amount: " + bonus);
         } 
         else {
-            System.out.println("The number " + number + " is not a natural number");
+            System.out.println("Bonus amount: 0");
         }
     }
 }
