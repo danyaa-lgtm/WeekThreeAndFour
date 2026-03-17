590c68e238ea24b94e86db14c95ec480ff7a9bb6
danyaa-lgtm <pro.danyaa@gmail.com>
2026-03-17 09:14:11 +0530

    Level1 Ques3

--- src/WeekThreeAndFour.java
+++ src/WeekThreeAndFour.java
@@ -12,10 +12,22 @@ public class WeekThreeAndFour {
         System.out.print("Enter third number: ");
         int number3 = sc.nextInt();
 
-        if (number1 < number2 && number1 < number3) {
-            System.out.println("Is the first number the smallest? Yes");
+        if (number1 > number2 && number1 > number3) {
+            System.out.println("Is the first number the largest? Yes");
         } else {
-            System.out.println("Is the first number the smallest? No");
+            System.out.println("Is the first number the largest? No");
+        }
+
+        if (number2 > number1 && number2 > number3) {
+            System.out.println("Is the second number the largest? Yes");
+        } else {
+            System.out.println("Is the second number the largest? No");
+        }
+
+        if (number3 > number1 && number3 > number2) {
+            System.out.println("Is the third number the largest? Yes");
+        } else {
+            System.out.println("Is the third number the largest? No");
         }
     }
 }
