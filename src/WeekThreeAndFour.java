1379fbc2a97756b520fc46b26e391464ca5314bf
danyaa-lgtm <pro.danyaa@gmail.com>
2026-03-17 09:27:46 +0530

    Level2 Ques1

--- src/WeekThreeAndFour.java
+++ src/WeekThreeAndFour.java
@@ -3,12 +3,19 @@ public class WeekThreeAndFour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number: ");
-        int n = sc.nextInt();
-        if (n > 0) {
-            int sum = n * (n + 1) / 2;
-            System.out.println("The sum of " + n + " natural numbers is " + sum);
-        } else {
-            System.out.println("The number " + n + " is not a natural number");
+        int number = sc.nextInt();
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is an even number");
+                } 
+                else {
+                    System.out.println(i + " is an odd number");
+                }
+            }
+        } 
+        else {
+            System.out.println("The number " + number + " is not a natural number");
         }
     }
 }
