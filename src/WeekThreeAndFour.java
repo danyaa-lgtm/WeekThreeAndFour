3f318ec461c964f45a8c743589ca88d629357909
danyaa-lgtm <pro.danyaa@gmail.com>
2026-03-17 09:23:48 +0530

    Level1 Ques5

--- src/WeekThreeAndFour.java
+++ src/WeekThreeAndFour.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 public class WeekThreeAndFour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter number: ");
         int n = sc.nextInt();
-
         if (n > 0) {
             int sum = n * (n + 1) / 2;
             System.out.println("The sum of " + n + " natural numbers is " + sum);
